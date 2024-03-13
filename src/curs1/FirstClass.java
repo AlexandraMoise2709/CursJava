41b89f0daa9ecbf6bdc838d1854b20cf9cb317e8
Alexandra <cernea.alexandra279@gmail.com>
2024-03-13 19:21:41 +0200

    curs1

---
+++ src/curs1/FirstClass.java
@@ -0,0 +1,19 @@
+package curs1;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+
+		FirstClass obiect = new FirstClass();  
+		obiect.greetings();
+	}
+
+	public void greetings() {
+		
+		System.out.println("Test");
+		//sysout + ctrl + space
+	}	
+	
+}
+
+
