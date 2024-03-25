36d30be419b9b83800449cbae35a91fdb02556d4
Alexandra <cernea.alexandra279@gmail.com>
2024-03-25 21:28:03 +0200

    curs5 final

---
+++ src/Curs5/Palindrom.java
@@ -0,0 +1,42 @@
+package Curs5;
+
+public class Palindrom {
+
+	public static void main(String[] args) {
+	
+		String cuvant = "r0adar";
+		String cuvantIntors = "";
+		
+		/*
+		 * cuvntul = radar -- >radar.lenght() = 5;
+		 * 
+		 * cuvantIntors = "";
+		 * cuvantIntors = cuvantIntors + cuvant.charAt(4);
+		 * 
+		 * ""= "" + r
+		 * 
+		 * cuvantIntors = cuvantIntors + cuvant.charAt(3);
+		 * cuvantIntors = cuvantIntors 
+		 * 
+		 * 
+		 */
+		
+		
+		for(int i = cuvant.length()-1; i>=0;i--) {
+			
+			
+			cuvantIntors = cuvantIntors + cuvant.charAt(i);
+			
+			}
+		
+		if (cuvant.equals(cuvantIntors)) {
+			
+			System.out.println("Este palindrom");
+		}else {
+			System.out.println("Nu Este palindrom");
+			
+		}
+		
+	}
+
+}
