b69f3be8eed4215fa5576a6f6814f2fd414bcccd
Alexandra <cernea.alexandra279@gmail.com>
2024-04-01 05:47:29 +0300

    corectii cursuri

--- src/curs6/CalculSalarial.java
+++ src/curs6/CalculSalarial.java
@@ -1,53 +1,67 @@
 package curs6;
-
-import java.util.Scanner;
-
 /*
- * calculeaza saariul pe 1 sapt
- * intreba utlizatorul cate ore a lucrat
+ * facem un program care calculeaza salariul pe o saptamana
+ * intrebam utilizatorul cate ore a lucrat
  * calculam doar daca a lucrat maxim 40 si minim 1
  * daca introduce un numar invalid il rugam sa introduca din nou
- * facem asta pana cand introduce un nr valid\
+ * facem asta pana introduce un numar valid
  * 
- * calculam cu formula salaiu= nr de ore lucrate*rate per hour 35
- * 
- * 
+ * calculam cu formula:
+ * Salariu = nr ore lucrate * rate pe hour
+ * ratePerHour = 35
  * 
  */
 
-
+import java.util.Scanner;
 
 public class CalculSalarial {
-	
+
 	static final int RATE_PER_HOUR = 35;
 	
-	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InterruptedException  {
+
 		Scanner scan = new Scanner(System.in);
-		int oreLucrate;
-		System.out.println("Cate ore ai lucrat");
+		System.out.println("Cate ore ai lucrat?");
+		int oreLucrate = scan.nextInt();
+
 		
-		for(oreLucrate = scan.nextInt(); oreLucrate <1 || oreLucrate > 40;oreLucrate = scan.nextInt()) {
-			
-			if(oreLucrate <= 40 || oreLucrate >=1) {
-				System.out.println("Salariul tau este: " + oreLucrate*RATE_PER_HOUR);
-				
-				break;
-			}else {
-				System.out.println("nr invalid");
-				oreLucrate = scan.nextInt();
-			
-			}
-			
+		while(oreLucrate <1 || oreLucrate >40) {
+			System.out.println("Nr invalid. Introdu numarul de ore:");
+			oreLucrate = scan.nextInt();
 		}
+		System.out.println("Salariul tau este : " + oreLucrate*RATE_PER_HOUR);
+		
+		
+	/*	int i = 3;
+		while(i > 0) {		
+			System.out.println("Programul se inchide in " + i + " secunde!");
+			Thread.sleep(1000);
+			i--;
+		}*/
+		
+		for(int i =3; i>0; i--) {
+			System.out.println("Programul se inchide in " + i + " secunde!");
+			Thread.sleep(1000);
+		}
+		
+		
+		/*for(oreLucrate = scan.nextInt(); oreLucrate < 1 || oreLucrate >40 ; oreLucrate = scan.nextInt()) {
+			
+			System.out.println("Nr invalid. Introdu numarul de ore:");
+
+		}
+		System.out.println("Nr invalid. Introdu numarul de ore:");*/
+
+	/*	
+		if(oreLucrate <=40 || oreLucrate >= 1) {
+			System.out.println("Salariul tau este : " + oreLucrate*RATE_PER_HOUR);
+			break;
+
+		}else {
+			System.out.println("Nr invalid. Introdu numarul de ore:");
+			oreLucrate = scan.nextInt();
+			
+		}*/
 	}
-	
-	
-	
-	
-	
-	
 
 }
-
