0055984e95cddbf79fb6114a56c61e6806251fc7
Alexandra <cernea.alexandra279@gmail.com>
2024-04-08 11:35:24 +0300

    Corectie curs8

---
+++ src/Curs8/TestRandomNumberGenerator.java
@@ -0,0 +1,14 @@
+package Curs8;
+
+public class TestRandomNumberGenerator {
+
+	public static void main(String[] args) {
+
+		RandomNumberGenerator obj =  new RandomNumberGenerator();
+		obj.addNrToArray();
+		obj.printArray();
+		
+		
+	}
+
+}
