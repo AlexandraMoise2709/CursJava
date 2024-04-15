4234109c7ebd99c9c8d7042e0ad4c1b40ebba4b9
Alexandra <cernea.alexandra279@gmail.com>
2024-04-15 20:29:50 +0300

    curs11 intermediar

---
+++ src/Curs11/AgeVerifier.java
@@ -0,0 +1,23 @@
+package Curs11;
+
+public class AgeVerifier {
+	
+	public void checkAgeLimit (int age) throws InvalidAgeException {
+		
+		if(age < 18) {
+			
+			
+			throw new InvalidAgeException("Invalid age to proceed!");
+			
+			
+		}else {
+			
+			System.out.println("You are allowd!");
+		}
+		
+		
+		
+		
+	}
+
+}
---
+++ src/Curs11/ExceptionExemple4.java
@@ -0,0 +1,10 @@
+package Curs11;
+
+public class ExceptionExemple4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
