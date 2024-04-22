c04c92622fa3919e6346be343c66be680cd5152a
AlexandraMoise2709 <163036555+AlexandraMoise2709@users.noreply.github.com>
2024-04-22 17:59:33 +0300

    Update SavingsAccount.java

--- src/HomeworkCurs11/SavingsAccount.java
+++ src/HomeworkCurs11/SavingsAccount.java
@@ -7,6 +7,9 @@ Suprascrie metoda withdraw unde implementeaza:
 - daca suma ce urmeaza a fi retrasa este mai mare decat balanta curenta,
 arunca InsuficientFundsException
 - altfel scade suma ce urmeaza a fi retrasa din balanta
+Daca userul nu are suficienti bani va arunca o exceptie si va printa : “The
+amount you entered is greater than the available balance :” + balance si
+”Thank you for using our ATM” si printam si stackTrace-ul exceptiei
  * 
  * 
  * 
@@ -27,7 +30,10 @@ public class SavingsAccount extends Account {
 		
 		if(suma < balance) {	
 			balance -=suma;
+			System.out.println("Please pick your money!");
+			System.out.println("Thank you for using our ATM");
 		}else {
+			System.out.println("Thank you for using our ATM");
 			throw new InsufficientFundsException("Insuficient funds for transaction");
 		} 
 			
